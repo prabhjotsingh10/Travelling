81d787cb54644e1b55f46fc8eb2cfcd94e01c7b7
prabhjotsingh10 <prabhjot.singh232@gmail.com>
2020-03-03 15:36:04 -0500

    removed additional port

--- app.js
+++ app.js
@@ -171,7 +171,9 @@ app.post("/sign_up", (req,res)=>{
       text: `Hello ${req.body.first_nme} ${req.body.last_nme}, welcome to Travelling Bud`,
     };
     sgMail.send(msg)
-   
+   .then(()=>{
+       res.redirect("dashboard");
+   })
    client.messages
      .create({
         body: `${req.body.first_nme} ${req.body.last_nme} Message: Welcome to TravellingBud Bro`,
@@ -180,7 +182,7 @@ app.post("/sign_up", (req,res)=>{
       })
      .then(message =>{ 
         console.log(message.sid);
-        res.redirect("dashboard");
+        res.render("dashboard");
      })
      
      }
